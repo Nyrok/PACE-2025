2a0cb41809bf77a7a9c231bc2911bb353825704e
Nyrok <47859726+Nyrok@users.noreply.github.com>
2025-12-14 19:23:11 +0100

    Implementation of the Greedy algorithm

--- src/src/solvers/solver.c
+++ src/src/solvers/solver.c
@@ -4,10 +4,12 @@ void	solve_graph(t_graph *graph) {
 	t_ull	i;
 
 	apply_degree_one_rule(graph);
-	printf("%lld\n", graph->v_count);
+	solve_greedy(graph);
+	printf("%lld\n", graph->len_solutions);
 	i = 0;
 	while (i < graph->v_count) {
-		printf("%lld\n", i + 1);
+		if (graph->solutions[i])
+			printf("%lld\n", i + 1);
 		i++;
 	}
 	free_graph(graph);
